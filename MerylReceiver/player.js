2bb8d814f027cb0324177739b0b83f4ed4409e53
Yury Pavlotsky <ypavlotsky@users.noreply.github.com>
2016-11-04 16:30:16 -0400

    Update player.js

--- MerylReceiver/player.js
+++ MerylReceiver/player.js
@@ -254,7 +254,7 @@ Player.prototype.onLoad = function(event) {
 Player.prototype.onSeek = function(event) {
   var currentTime = event.data.currentTime;
   this.snapback_(currentTime);
-  this.broadcastStatus(false, event.data.requestId);
+  this.mediaManager_.broadcastStatus(false, event.data.requestId);
 };
 
 /**
